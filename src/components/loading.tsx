814cb426675d8064ca45777ba12bf494c2af4df0
Gabrielle Oliveira <oliveiragabrielle662@gmail.com>
2024-04-30 19:45:25 -0300

    semana 5

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,9 +1,14 @@
+import LottieView from "lottie-react-native";
 import { ActivityIndicator, View } from "react-native";
 
 export function Loading() {
     return (
-        <View className="justify-center items-center mt-20">
-            <ActivityIndicator />
-        </View>
+            <LottieView
+                source={require('../assets/loading-animate.json')}
+                style={{width: "50%", height: "50%"}}
+                autoPlay 
+                loop
+            />
+            
     )
 }
